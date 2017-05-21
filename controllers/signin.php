cba6ccdc33c2d0da0bd8e0822d343904eaab3d7b
Tim Flaherty <tflahertytmf@gmail.com>
2017-05-20 21:07:36 -0700

    Protect against SQL injection

--- controllers/signin.php
+++ controllers/signin.php
@@ -1,8 +1,11 @@
 <?php
+	include("../lib/val.php");
 // If input is POSTed, assign variables & input into database
 	if(isset($_POST['visitor'])) {
 		$visitor = $_POST['visitor'];
+		$visitor = noInject($visitor);
 		$visiting = $_POST['visiting'];
+		$visiting = noInject($visiting);
 		$timein = date("Y-m-d H:i:s");
 		//Insert into database
 		$insert = "INSERT INTO log (name, visiting) VALUES ('".$visitor."', '".$visiting."')";
--- controllers/signout.php
+++ controllers/signout.php
@@ -1,7 +1,9 @@
 <?php
 	include('../models/dbconnect.php');
+	include("../lib/val.php");
 	//Grab data sent by AJAX
 	$x = $_REQUEST["x"];
+	$x = noInject($x);
 	$outtime = date("Y-m-d H:i:s");
 	//Insert out time into database
 	$insertOut = "UPDATE log SET timeout ='".$outtime."' WHERE name = '".$x."'";
